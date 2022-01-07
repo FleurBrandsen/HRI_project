20e854ac2fb0ef7253e01e49a86a85393ea0b57a
Jort.Gutter <jort.gutter@ru.nl>
2022-01-07 10:34:12 +0100

    changes

--- controllers/tiago_python_controller/tiago_python_controller.py
+++ controllers/tiago_python_controller/tiago_python_controller.py
@@ -40,7 +40,7 @@ class MyRobot(Robot):
         self.keyboard.enable(self.timeStep)
         self.keyboard = self.getKeyboard()
         
-        # Camera of Tiago (currently not working)
+        # Camera
         self.camera = self.getDevice('camera')
         self.camera.enable(self.timeStep)
         print("Camera: ", self.camera)
@@ -56,7 +56,30 @@ class MyRobot(Robot):
             print(i, self.getDeviceByIndex(i))
         
         #Cameras from external source (MultiSenseS21)
-        self.inertialunit = self.getDeviceByIndex(42)
+        #self.inertialunit = self.getDeviceByIndex(42)
+        
+        #for i in range(self.getNumberOfDevices()):
+        #    break
+        #    print(i, self.getDeviceByIndex(i))
+        
+
+        # Head motors:
+        #self.head_horizontal = self.getMotor("head_1_joint")
+        #self.head_vertical = self.getMotor("head_2_joint")
+        
+        #self.horizontal_sensor = self.head_horizontal.getPositionSensor()
+        #self.horizontal_sensor.enable(self.timeStep)
+        #self.vertical_sensor = self.head_vertical.getPositionSensor()
+        #self.vertical_sensor.enable(self.timeStep)
+        
+        # Wheel motors
+        #self.wheel_left = self.getMotor("wheel_left_joint")
+        #self.wheel_right = self.getMotor("wheel_right_joint")
+        
+        #self.wheel_left_sensor = self.wheel_left.getPositionSensor()
+        #self.wheel_left_sensor.enable(self.timeStep)
+        #self.wheel_right_sensor = self.wheel_right.getPositionSensor()
+        #self.wheel_right_sensor.enable(self.timeStep)
         #print(self.inertialunit.SFRotation)
         
         #self.leftcamera = self.getDeviceByIndex(43) 
@@ -72,6 +95,22 @@ class MyRobot(Robot):
         
         # actuators:
         self.actuators = {
+            'head_horizontal':{  # Head
+                'motor': self.getDevice('head_1_joint'),
+                'sensor': self.getDevice('head_1_joint_sensor')
+            },
+            'head_vertical':{
+                'motor': self.getDevice('head_2_joint'),
+                'sensor': self.getDevice('head_2_joint_sensor')
+            },
+            'wheel_left':{
+                'motor': self.getDevice('wheel_left_joint'),
+                'sensor': self.getDevice('wheel_left_joint_sensor')
+            },
+            'wheel_right':{
+                'motor': self.getDevice('wheel_right_joint'),
+                'sensor': self.getDevice('wheel_right_joint_sensor')
+            },
             'left_hand_left_finger': {  # Hands
                 'motor': self.getDevice('left_hand_gripper_left_finger_joint'),
                 'sensor': self.getDevice('left_hand_gripper_left_finger_joint_sensor')
@@ -145,14 +184,41 @@ class MyRobot(Robot):
                 'sensor': None
             }
         }
-        # add extra values to actuator dictionary:
+        # add extra values to actuator dictionary and enable sensors:
         for actuator in self.actuators.values():
             actuator['last_pos'] = 0
             actuator['moving'] = False
-        
-                
-        
-
+            if actuator['sensor']:
+                actuator['sensor'].enable(self.timeStep)
+            
+            
+        # checking routine:
+        self.checks = {
+            0: {
+                'actuator': 'head_horizontal',
+                'change': 0.1 
+            },
+            1: {
+                'actuator': 'head_vertical',
+                'change': -0.1 
+            },
+            2:{
+                'actuator': 'head_vertical',
+                'change': 0.1 
+            },
+            3:{
+                'actuator': 'head_horizontal',
+                'change': -0.1 
+            },
+            4:{
+                'actuator': 'head_vertical',
+                'change': 0.1 
+            },
+            5:{
+                'actuator': 'head_vertical',
+                'change': -0.1 
+            }
+        }
         
     def camera_read_external(self):
         img = []
@@ -164,99 +230,7 @@ class MyRobot(Robot):
             img = cv2.resize(img, None, fx=0.5, fy=0.5) # image downsampled by 2
                         
         return img
-            
-    # Displays the image on the webots camera display interface
-    def image_to_display(self, img):
-        if self.ext_camera:
-            height, width, channels = img.shape
-            imageRef = self.displayCamExt.imageNew(cv2.transpose(img).tolist(), Display.RGB, width, height)
-            self.displayCamExt.imagePaste(imageRef, 0, 0)
-        
-    def printHelp(self):
-        print(
-            'Commands:\n'
-            ' H for displaying the commands\n'
-            ' UP for green\n'
-            ' Down for black\n'
-            ' Left for blue\n'
-            ' Right for red'
-        )
-        
-    def get_colour_mask(self, colour):
-        # Mask possibilities: red, blue, green, black
-        # colour is a list with bgr values [b, g, r]
-                
-        #use the camera
-        self.camera = self.leftcamera
-        
-        while self.step(self.timeStep) != -1:       
-            #Credits to Niels Cornelissen on the discord
-            w = self.camera.getWidth()
-            h = self.camera.getHeight()
-            img = self.camera.getImage()
-            img = Image.frombytes('RGBA', (w, h), img, 'raw', 'BGRA')
-            img = np.array(img.convert('RGB'))
-            img = cv2.resize(img, None, fx=2, fy=2)
-            self.image_to_display(img)
-            
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            bgr = colour
-            green_HSV = cv2.cvtColor(np.uint8([[bgr]] ), cv2.COLOR_BGR2HSV)[0][0]
-
-            #40 is chosen as +- cutoff, can change
-            lower = np.array([green_HSV[0] - 40, green_HSV[1] - 40, green_HSV[2] - 40])
-            upper = np.array([green_HSV[0] + 40, green_HSV[1] + 40, green_HSV[2] + 40])
-
-            mask = cv2.inRange(img, lower, upper)
-
-            m = cv2.moments(mask)
-            #If colour is in fov, get location of colour on the camera
-            if m["m00"] != 0:
-                cx, cy = int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"])
-                print(cx,cy)
-                # Need to find a better solution to ending this loop
-                # Want it to end when another button is pressed
-                break
-        # Disable the camera after use     
-        self.camera.disable()
-    
-    def choose_colour(self, input):
-    # choose which colour, which then is set in BGR
-        if input == 'red':
-            colour = [0, 0, 255]
-        elif input == 'blue':
-            colour = [255, 0, 0]
-        elif input == 'green':
-            colour = [65, 229, 158] #[0, 255, 0]?
-        elif input == 'black':
-            colour = [0, 0, 0]   
-        
-        self.get_colour_mask(colour)
-        return colour
-        
-    def run_keyboard(self):
-        dict = {'red': [0, 0, 255]}
-        # Main loop.
-        while True:
-            # Deal with the pressed keyboard key.
-            k = self.keyboard.getKey()
-            message = ''
-            if k == ord('H'):
-                self.printHelp()
-            elif k == ord('C'):
-                self.use_camera()
-            elif k == Keyboard.UP:
-                print("green: ", self.choose_colour('green'))
-            elif k == Keyboard.DOWN:
-                print("black: ", self.choose_colour('black'))
-            elif k == Keyboard.LEFT:
-                print('blue: ', self.choose_colour('blue'))
-            elif k == Keyboard.RIGHT:
-                print('red: ', self.choose_colour('red'))
-            
-            if self.step(self.timeStep) == -1:
-                break
-                
+ 
     # Object manipulation:
                 
     def grab(self, side):
@@ -371,7 +345,279 @@ class MyRobot(Robot):
             pass
             
         
+        
+    def printHelp(self):
+        print(
+            'Commands:\n'
+            ' H for displaying the commands\n'
+            ' UP for green\n'
+            ' Down for pink (?)\n'
+            ' Left for blue\n'
+            ' Right for red\n'
+            ' O for turning left\n'
+            ' P for turning right\n'
+            ' I for information on wheels\n'
+            ' L for stopping turning\n'
+            ' U for positional values of the head\n'
+            ' W for turning the head upwards\n'
+            ' A for turning the head to the left\n'
+            ' S for turning the head downwards\n'
+            ' D for turning the head to the right\n'
+        )
+
+    def get_colour_mask(self, colour, colour_name):
+        # Mask possibilities: red, blue, green, black
+        # colour is a list with bgr values [b, g, r]
+        
+        self.camera.enable(self.timeStep)
+        checked = 0
+        found = False
+        cutoff = 250 #360
+        middle = 250
+        
+        while self.step(self.timeStep) != -1: 
+            #40 is chosen as +- cutoff, can change
+            colour_HSV = colour
+            boundary = 40
+            lower = np.array([colour_HSV[0] - boundary, colour_HSV[1] - boundary, colour_HSV[2] - boundary])
+            upper = np.array([colour_HSV[0] + boundary, colour_HSV[1] + boundary, colour_HSV[2] + boundary])
+            
+            while self.step(self.timeStep) != -1:
+                #Credits to Niels Cornelissen on the discord
+                img = self.get_camera_image()
+                
+                mask = cv2.inRange(img, lower, upper)
+                m = cv2.moments(mask)
+                
+                #If colour is in fov, get location of colour on the camera
+                if m["m00"] != 0:
+                    cx, cy = int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"])
+                    print(f"Found the {colour_name} box at: ", cx,cy)\
+                    #Turn cx, cy into head positioning (cx, cy in the middle of the screen)
+                    #left-down = 0, 500, right-down = 500, 500
+                    #left-up should be 0, 0, right up should be 500, 0
+                    
+                    x_speed = abs(middle- cx)/middle
+                    if not found:
+                        if cx > cutoff:
+                            input = -0.1
+                        elif cx < cutoff:
+                            input = 0.1
+                        else:
+                            input = 0
+                        self.move_joint('head_horizontal', input*x_speed)    
+                        
+                        y_speed = abs(middle - cy)/middle
+                        if cy > cutoff: 
+                            input = -0.1
+                        elif cy < cutoff:
+                            input = 0.1
+                        else:
+                            input = 0
+                        self.move_joint('head_vertical', input*y_speed)
+                        if cx == cutoff and cy == cutoff:
+                            found = True
+                            self.actuators['head_horizontal']['motor'].setPosition(0)
+                            self.actuators['head_horizontal']['motor'].setVelocity(0.15)
+                            self.turn_to_can(cx, turn_speed = x_speed)
+                            #break
+                
+                    if found:
+                        test = self.turn_to_can(cx)
+                        hor_pos = self.actuators['head_horizontal']['sensor'].getValue()  
+                        print(hor_pos)                     
+                        if 240 < cx < 260 and -0.1 < hor_pos < 0.1:
+                            self.actuators['wheel_left']['motor'].setVelocity(0)
+                            self.actuators['wheel_right']['motor'].setVelocity(0)
+                            break
+                    
+                    # Need to find a better solution to ending this loop
+                    # Want it to end when another button is pressed
+                    #break
+                
+                #Searching for the colour
+                else:
+                    if checked < 6:
+                        # joint, position, input = self.search_routine(checked)
+                        check_stage = self.checks[checked]
+                        checked += self.move_joint(check_stage['actuator'], check_stage['change'])
+                    else:
+                        print(f"{colour_name} not found")
+                        break
+                    
+                        
+            break
+            
+    def get_camera_image(self):
+        #Credits to Niels Cornelissen on the discord
+        w = self.camera.getWidth()
+        h = self.camera.getHeight()
+        img = self.camera.getImage()
+        img = Image.frombytes('RGBA', (w, h), img, 'raw', 'BGRA') 
+        img = np.array(img.convert('RGB'))
+        img = cv2.resize(img, None, fx=2, fy=2)
+        
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        return img
+    
+    
+    def choose_colour(self, input):
+    # choose which colour, which then is set in BGR
+        if input == 'red':
+            colour = [255,0,0]
+        elif input == 'blue':
+            colour = [0,0,255] # [207, 98, 100]
+        elif input == 'green':
+            colour =  [0,255,0] # [65, 229, 158] 
+        elif input == 'yellow':
+            colour = [int(255*0.9),int(255*0.9),int(255*0.2)] 
+        # BGR and RGB are switched around for some reason
+        colour = cv2.cvtColor(np.uint8([[colour]] ), cv2.COLOR_BGR2HSV)[0][0]
+        print("Colour: ", colour)
+        print(f"Searching for {input}")
+        self.get_colour_mask(colour, input)
+        return colour
+    
+    def search_routine(self, checked):
+        if checked == 0:
+            joint = self.head_horizontal
+            position = self.horizontal_sensor.getValue()
+            input = 0.1     
+        elif checked == 1:
+            joint = self.head_vertical
+            position = self.vertical_sensor.getValue()
+            input = -0.1
+        elif checked == 2:
+            joint = self.head_vertical
+            position = self.vertical_sensor.getValue()
+            input = 0.1    
+        elif checked == 3:
+            joint = self.head_horizontal
+            position = self.horizontal_sensor.getValue()
+            input = -0.1
+        elif checked == 4:
+            joint = self.head_vertical
+            position = self.vertical_sensor.getValue()
+            input = 0.1
+        elif checked == 5:
+            joint = self.head_vertical
+            position = self.vertical_sensor.getValue()
+            input = -0.1
+        
+        return joint, position, input
+    
+    def turn_to_can(self, cx, turn_speed = 0.1):
+        print(cx)
+        if cx > 270:
+            self.turn_body(-1, turn_speed)
+        elif cx < 230:
+            self.turn_body(1, turn_speed)
+        else:
+            self.actuators['wheel_left']['motor'].setPosition(0)
+            self.actuators['wheel_right']['motor'].setPosition(0)
+        
+    def move_joint(self, joint, input):
+        motor = self.actuators[joint]['motor']
+        sensor = self.actuators[joint]['sensor']
+        if(motor.getMaxPosition() > (sensor.getValue() + input) > motor.getMinPosition()):
+                    motor.setPosition(float(sensor.getValue() + input))
+                    motor.setVelocity(1)
+                    return 0
+        else:
+            return 1    
+            
+    def turn_body(self, input, turn_speed = 0.1):
+        wheel_l = self.actuators['wheel_left']
+        wheel_r = self.actuators['wheel_right']
+        wheel_l['motor'].setVelocity(1)
+        wheel_r['motor'].setVelocity(1)
+        # Turn left
+        if input > 0:
+            wheel_l['motor'].setPosition(wheel_l['sensor'].getValue()-turn_speed)
+            wheel_r['motor'].setPosition(wheel_r['sensor'].getValue()+turn_speed)
+        # Turn right
+        else:
+            wheel_l['motor'].setPosition(wheel_l['sensor'].getValue()+turn_speed)
+            wheel_r['motor'].setPosition(wheel_r['sensor'].getValue()-turn_speed)
+        return
+        
+        
+    def run_keyboard(self):
+        dict = {'red': [0, 0, 255]}
+            
+        # Main loop.
+        while True:
+            # Deal with the pressed keyboard key.
+            k = self.keyboard.getKey()
+            message = ''
+            if k == ord('H'):
+                self.printHelp()
+            elif k == ord('C'):
+                self.use_camera()
+            elif k == Keyboard.UP:
+                print("green: ", self.choose_colour('green'))
+            elif k == Keyboard.DOWN:
+                print("yellow: ", self.choose_colour('yellow'))
+            elif k == Keyboard.LEFT:
+                print('blue: ', self.choose_colour('blue'))
+            elif k == Keyboard.RIGHT:
+                print('red: ', self.choose_colour('red'))
+            
+            #Rotate head up (W), left (A), down (S), right (D)
+            elif k == ord('A'):
+                # Rotate to the left
+                self.move_joint('head_horizontal', 0.1)
+                
+            elif k == ord('D'):
+                # Rotate to the right
+                self.move_joint('head_horizontal', -0.1) 
+            elif k == ord('W'):
+                #Rotate upwards 
+                self.move_joint('head_vertical', 0.1)
+            elif k == ord('S'):
+                #Rotate  downwards
+                self.move_joint('head_vertical', -0.1)
+            
+            # Something weird is happening here. They either both go to the right
+            # or both go to the left.
+            elif k == ord('O'):
+                #Rotate body to the left
+                print("Turning left")
+                self.turn_body(1)
+                
+            elif k == ord('P'):
+                #Rotate  body to the right
+                print("Turning right") 
+                self.turn_body(-1)
+                           
+            elif k == ord("I"):
+                print("Info:")
+                print("Min-pos: ", self.actuators['wheel_left']['motor'].getMinPosition())
+                print("Max-pos: ", self.actuators['wheel_left']['motor'].getMaxPosition())
+                print("Acceleration: ", self.actuators['wheel_left']['motor'].getAcceleration())
+                print("Velocity: ", self.actuators['wheel_left']['motor'].getVelocity())
+                print("Max-Velocity: ", self.actuators['wheel_left']['motor'].getMaxVelocity())
+                print("Left Wheel Position: ", self.actuators['wheel_left']['sensor'].getValue())
+                print("Right Wheel Position: ", self.actuators['wheel_right']['sensor'].getValue())
+                print("Einde info")
+                
+            elif k == ord("U"):
+                print("Head joint horizontal: ", self.actuators['head_horizontal']['sensor'].getValue())
+                print("Head joint vertical: ", self.actuators['head_vertical']['sensor'].getValue())
+                
+            elif k == ord("L"):
+                print("Stopping")
+                self.wheel_left.setPosition(0)
+                self.wheel_right.setPosition(0)
+                self.wheel_left.setVelocity(0)
+                self.wheel_right.setVelocity(0)
+
+            
+            if self.step(self.timeStep) == -1:
+                break
+                
+
 
 robot = MyRobot(ext_camera_flag = False)
-#robot.run_keyboard()
-robot.test_hands()
+robot.run_keyboard()
+#robot.test_hands()
